6db4b239671d347d79d852f0b33d60a4387c377e
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-02-26 10:43:46 -0500

    Building user registration screen, new gradle implementations

--- FirebaseApp/app/src/main/java/com/chandlerg/firebaseapp/RegisterActivity.java
+++ FirebaseApp/app/src/main/java/com/chandlerg/firebaseapp/RegisterActivity.java
@@ -3,10 +3,16 @@ package com.chandlerg.firebaseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.ActionBar;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    // views
+    EditText mEmailEt, mPasswordEt;
+    Button mRegistrationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,7 @@ public class RegisterActivity extends AppCompatActivity {
         // Actionbar and its title
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Create Account");
+        //enable back button
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
     }
